ef203c0a1b31cf14bbd2efc14ae1ebe740b7625f
Brad Alexander <brad.alexander913@gmail.com>
2019-08-04 21:23:18 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,12 +1,19 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      :patriarch => ,
-      #your key/value pairs here!
-      }, 
-    :capulet => {
-      #your key/value pairs here!
-      }}
+      :montague => {
+        :patriarch => {},
+        :matriarch => {},
+        :hero => {},
+        :hero_friends => []
+      },
+      :capulet => {
+        :patriarch => {},
+        :matriarch => {},
+        :heroine => {},
+        :heroine_friends => []
+      }
+    
+  }
 
   
 end
